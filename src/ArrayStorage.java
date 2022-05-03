c2f62e4acb85bfb57aef583f668d2811c6cedb2c
Dzianis Kazakevich <kazakevichdev@gmail.com>
2022-05-03 15:36:56 +0300

    HW_1 Implemented method clear, save, get, delete, size in ArrayStorage.java

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,55 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int SIZE = 0;
 
     void clear() {
+        if (SIZE == 0) {
+            System.out.println("Array is clear");
+        } else {
+            Arrays.fill(storage, 0, SIZE, null);
+            SIZE = 0;
+        }
     }
 
     void save(Resume r) {
+        if (SIZE < 10000) {
+            storage[SIZE] = r;
+            SIZE++;
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < SIZE; i++) {
+            if (storage[i].uuid == uuid) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < SIZE; i++) {
+            if (storage[i].uuid == uuid) {
+                storage[i] = storage[SIZE - 1];
+                storage[SIZE - 1] = null;
+                SIZE--;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOfRange(storage, 0, SIZE);
     }
 
     int size() {
-        return 0;
+        return SIZE;
     }
 }
