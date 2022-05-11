5dbc00e8c1fea90132bba428b4548009af6afaba
Dzianis Kazakevich <kazakevichdev@gmail.com>
2022-05-11 13:45:25 +0300

    HW_2 ArrayStorage.java refactoring after code review

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -8,8 +8,8 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    static final int STORAGE_LIMIT = 10000;
-    private Resume[] storage = new Resume[STORAGE_LIMIT];
+    protected static final int STORAGE_LIMIT = 10000;
+    private final Resume[] storage = new Resume[STORAGE_LIMIT];
     private int size = 0;
 
     public void clear() {
@@ -19,20 +19,22 @@ public class ArrayStorage {
 
     public void save(Resume r) {
         if (size < STORAGE_LIMIT) {
-            int position = findByUuid(r.getUuid());
-            if (position != -1) {
+            int index = getIndex(r.getUuid());
+            if (index != -1) {
                 System.out.println("Resume with uuid: " + r.getUuid() + " is existing.");
             } else {
                 storage[size] = r;
                 size++;
             }
+        } else {
+            System.out.println("Storage overflow!");
         }
     }
 
     public void update(Resume r) {
-        int position = findByUuid(r.getUuid());
-        if (position != -1) {
-            storage[position] = r;
+        int index = getIndex(r.getUuid());
+        if (index != -1) {
+            storage[index] = r;
             System.out.println("Resume with uuid: " + r.getUuid() + " was updated");
         } else {
             System.out.println("Resume with uuid: " + r.getUuid() + " doesn't exist.");
@@ -40,9 +42,9 @@ public class ArrayStorage {
     }
 
     public Resume get(String uuid) {
-        int position = findByUuid(uuid);
-        if (position != -1) {
-            return storage[position];
+        int index = getIndex(uuid);
+        if (index != -1) {
+            return storage[index];
         } else {
             System.out.println("Resume with uuid: " + uuid + " doesn't exist.");
             return null;
@@ -51,10 +53,10 @@ public class ArrayStorage {
     }
 
     public void delete(String uuid) {
-        int position = findByUuid(uuid);
-        if (position != -1) {
+        int index = getIndex(uuid);
+        if (index != -1) {
             size--;
-            storage[position] = storage[size];
+            storage[index] = storage[size];
             storage[size] = null;
         } else {
             System.out.println("Resume with uuid: " + uuid + " doesn't exist.");
@@ -72,7 +74,7 @@ public class ArrayStorage {
         return size;
     }
 
-    private int findByUuid(String uuid) {
+    private int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].getUuid().equals(uuid)) {
                 return i;
