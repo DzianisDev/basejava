14d0eaac5483fd71ed6e1a8f10cb680ae933f56f
Dzianis Kazakevich <kazakevichdev@gmail.com>
2022-08-24 12:08:47 +0300

    HW_5 | Implemented AbstractArrayStorageTest.java

---
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -0,0 +1,28 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.exception.StorageException;
+import com.urise.webapp.model.Resume;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public abstract class AbstractArrayStorageTest extends AbstractStorageTest {
+
+    public AbstractArrayStorageTest(Storage storage) {
+        super(storage);
+    }
+
+    @Test(expected = StorageException.class)
+    public void saveOverflow() {
+        try {
+            for (int i = 4; i <= AbstractArrayStorage.STORAGE_LIMIT; i++) {
+                storage.save(new Resume());
+            }
+        } catch (StorageException e) {
+            fail();
+        }
+
+        storage.save(new Resume());
+    }
+}
