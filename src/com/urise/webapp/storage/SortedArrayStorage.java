e92681f22ce8c2b727da6e3a57a864a43d23ab2d
Dzianis Kazakevich <kazakevichdev@gmail.com>
2022-08-25 12:59:10 +0300

    Deleted comparable interface in Resume.java and added comparator in SortedArrayStorage.java

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -3,13 +3,20 @@ package com.urise.webapp.storage;
 import com.urise.webapp.model.Resume;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
 
+    private static final Comparator<Resume> RESUME_COMPARATOR = new Comparator<Resume>() {
+        @Override
+        public int compare(Resume o1, Resume o2) {
+            return o1.getUuid().compareTo(o2.getUuid());
+        }
+    };
     @Override
     protected Integer getSearchKey(String uuid) {
         Resume searchKey = new Resume(uuid);
-        return Arrays.binarySearch(storage, 0, size, searchKey);
+        return Arrays.binarySearch(storage, 0, size, searchKey, RESUME_COMPARATOR);
     }
 
     @Override
@@ -23,4 +30,5 @@ public class SortedArrayStorage extends AbstractArrayStorage {
         System.arraycopy(storage, index, storage, index + 1, size - index);
         storage[index] = r;
     }
+
 }
